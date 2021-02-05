0abe8082ce97ff146bb87b7c84c9d9ccf236077d
Samba Askary <samba.askary-consultant@prudential.co.id>
2021-02-05 14:34:51 +0700

    MASTER

---
+++ src/Contact.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+ 
+class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>Contact</h2>
+                <p>
+                    Email: blog.cacan.id@gmail.com <br />
+                    Blog: http://blog.cacan.id
+                </p>
+            </div>
+        );
+    }
+}
+ 
+export default Contact;
